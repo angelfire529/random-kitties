d2fb6300ac90af694a3b441b6ec5368f340aa0fe
jamie broberg <angelfire529@yahoo.com>
2019-04-14 16:04:07 -0700

    adding production build
    adding build folder

---
+++ build/precache-manifest.bb2ccc859b44e9961ee20cf09b418cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "696c6e704ac97139bd41",
+    "url": "/random-kitties/static/js/runtime~main.2e73c38e.js"
+  },
+  {
+    "revision": "6ffc45b4673510496b0e",
+    "url": "/random-kitties/static/js/main.b938da45.chunk.js"
+  },
+  {
+    "revision": "b4376cc34714a6f99f27",
+    "url": "/random-kitties/static/js/2.34743270.chunk.js"
+  },
+  {
+    "revision": "6ffc45b4673510496b0e",
+    "url": "/random-kitties/static/css/main.9c3555c8.chunk.css"
+  },
+  {
+    "revision": "b4376cc34714a6f99f27",
+    "url": "/random-kitties/static/css/2.aeb423cf.chunk.css"
+  },
+  {
+    "revision": "1fb96bcd2a9ec1ae615c0fcb65f8cade",
+    "url": "/random-kitties/index.html"
+  }
+];
