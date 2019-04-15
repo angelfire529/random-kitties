092c59e2cb447ea0b61986ea56d5f8ccbd46bdf7
jamie broberg <angelfire529@yahoo.com>
2019-04-14 22:55:57 -0700

    adding check

--- src/service/cat-service.js
+++ src/service/cat-service.js
@@ -30,7 +30,7 @@ class CatService {
     }
 
     GetImagesByCategory(category) {
-        let cats = JSON.parse(localStorage.getItem('categories')) || [];
+        let cats = JSON.parse(localStorage.getItem('categories'))|| [];
         const found = cats.find(cat => {
             return cat.category.toLowerCase() === category.toLowerCase();
         });
@@ -67,7 +67,7 @@ class CatService {
     }
 
     Exists(category) {
-        let existing = JSON.parse(localStorage.getItem('categories'))
+        let existing = JSON.parse(localStorage.getItem('categories')) || []
         const found = existing.find(e => { return e.category.toLowerCase() === category.toLowerCase()});
         return  found ? true : false;
     }
